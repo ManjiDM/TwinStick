19bcec0bf102fe8aebfe622b81047bdd7a02bc52
Daniel Mota <Daniel Mota>
2016-06-11 13:31:08 +0100

    Added Materials _Scenes Textures & Scripts folder

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log ("H: " + CrossPlatformInputManager.GetAxis ("Horizontal") + "- V:" + CrossPlatformInputManager.GetAxis ("Vertical"));
+
+	}
+
+}
