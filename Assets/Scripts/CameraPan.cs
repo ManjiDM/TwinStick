6f8cfabc5a3ce6f11b190600c4e2e6fbe16cce5f
Daniel Mota <Daniel Mota>
2016-06-13 09:23:34 +0100

    Weird second stick implementation

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour
+{
+	// The target we are following
+	private Transform target;
+	// Use this for initialization
+	void Start ()
+	{
+		target = GameObject.FindGameObjectWithTag ("Player").GetComponent <Transform> ();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		if (!target)
+			return;
+		print ("RHorizontal " + Input.GetAxis ("RHorizontal"));
+		print ("RVertical " + Input.GetAxis ("RVertical"));
+		transform.LookAt (target);
+	}
+
+}
---
+++ Assets/Scripts/SelfieStick.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfieStick : MonoBehaviour
+{
+	[SerializeField]
+	private float _panSpeed = 10f;
+	private Transform target;
+	private Vector3 armRotation;
+	// Use this for initialization
+	void Start ()
+	{
+		target = GameObject.FindGameObjectWithTag ("Player").GetComponent <Transform> ();
+		armRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		armRotation.y += Input.GetAxis ("RHorizontal") * panSpeed;
+		armRotation.x += Input.GetAxis ("RVertical") * panSpeed;
+
+
+		transform.position = target.position;
+		transform.rotation = Quaternion.Euler (armRotation);
+	}
+
+	public float panSpeed {
+		get {
+			return _panSpeed;
+		}
+		set {
+			_panSpeed = value;
+		}
+	}
+}
